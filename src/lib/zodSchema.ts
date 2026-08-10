7c0234732409d329555c1bdbc47d6810245769ce
Abhijit Saha <work.abhijitsaha@gmail.com>
2026-08-10 23:27:54 +0530

    fix(validation): remove password length ceiling

--- src/lib/zodSchema.ts
+++ src/lib/zodSchema.ts
@@ -5,10 +5,9 @@ export const loginFormSchema = z.object({
     .string()
     .min(8, { error: "Your Id can not less than 8 charecters" })
     .max(88, { error: "Your Id can not more than 8 charecters" }),
-  password: z
-    .string()
-    .min(6, { error: "Your password is not less than 6 charecter" })
-    .max(12, { error: "Your password is not more than 12 charecters" }),
+  password: z.string().min(8, {
+    error: "Password must be at least 8 characters",
+  }),
 });
 
 export const AddTeacherAndStudentForm = z.object({});
