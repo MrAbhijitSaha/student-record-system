66e03b246251715de9b261afc40dd03aaa2640a8
Abhijit Saha <work.abhijitsaha@gmail.com>
2026-08-10 23:40:36 +0530

    fix(auth): redirect logged-in users to their profile

--- src/app/(public)/auth/login/page.tsx
+++ src/app/(public)/auth/login/page.tsx
@@ -1,7 +1,32 @@
 import LoginForm from "@/components/Forms/LoginForm";
 import { Card, CardContent } from "@/components/shadcnui/card";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    const role = session.user.role;
+
+    if (role === "admin") {
+      redirect("/admin/profile");
+    }
+
+    if (role === "student") {
+      redirect("/student/profile");
+    }
+
+    if (role === "teacher") {
+      redirect("/teacher/profile");
+    }
+
+    redirect("/");
+  }
+
   return (
     <div className="flex h-dvh w-full flex-col items-center justify-evenly md:flex-row">
       <div className="w-full text-center md:w-[50%]">
