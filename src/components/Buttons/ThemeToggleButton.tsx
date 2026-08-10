ee32c2b9d9825edb9a223d9f72da190e231fa3a1
Abhijit Saha <work.abhijitsaha@gmail.com>
2026-08-10 23:06:16 +0530

    fix: use resolved theme for toggle

--- src/components/Buttons/ThemeToggleButton.tsx
+++ src/components/Buttons/ThemeToggleButton.tsx
@@ -4,21 +4,21 @@ import { MoonIcon, SunDimIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 
 const ThemeToggleButton = () => {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
 
   return (
     <button
       type="button"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="flex cursor-pointer items-center">
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      className="relative flex cursor-pointer items-center">
       <SunDimIcon
         size={20}
-        className="-rotate-90 opacity-100 transition-all duration-300 dark:rotate-0 dark:opacity-0"
+        className="absolute rotate-90 opacity-0 transition-all duration-300 dark:rotate-0 dark:opacity-100"
       />
 
       <MoonIcon
         size={20}
-        className="absolute -rotate-90 opacity-0 transition-all duration-300 dark:rotate-0 dark:opacity-100"
+        className="rotate-0 opacity-100 transition-all duration-300 dark:-rotate-90 dark:opacity-0"
       />
     </button>
   );
