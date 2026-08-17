396e498e7a1832ed5c2a39973bfe70bae78439d5
Abhijit Saha <work.abhijitsaha@gmail.com>
2026-08-17 13:32:40 +0530

    Update person.ts

--- src/server/person.ts
+++ src/server/person.ts
@@ -280,7 +280,7 @@ export async function createTeacherOrStudent(
 
     return {
       success: false,
-      message: error instanceof Error ? error.message : "Something went wrong.",
+      message: "Something went wrong. Please try again.",
     };
   }
 }
